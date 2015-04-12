264dfd5b960ca28988e16dc2346b77b03d25c36a
erickcouto <ecouto123@gmail.com>
2015-04-12 20:27:00 -0300

    ajustes css

--- js/slide.js
+++ js/slide.js
@@ -24,11 +24,23 @@ $(document).ready(function(){
 
     $('.slide p').hide().html(texto).delay(500).fadeIn();
 
+
   }
 
 });
 
-$(window).resize(function(){
+window.addEventListener('resize', function(){
   var height = $('.slide img').height();
   $('.slide').height(height);   
+});
+
+function doSomething() {
+  var height = $('.slide img').height();
+  $('.slide').height(height); 
+};
+
+var resizeTimer;
+$(window).resize(function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(doSomething, 100);
 });
